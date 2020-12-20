0029b42062ffa0f298cd0771ad117b7079b8e076
StarparkX <75814315+StarparkX@users.noreply.github.com>
2020-12-20 15:11:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ Parse.Cloud.useMasterKey();
 
 Parse.serverURL = 'https://starpark.herokuapp.com/parse'
 //var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
-var databaseUri = "mongodb+srv://heroku_h5sc1hpx:jer7765m@cluster-h5sc1hpx.enbxn.mongodb.net/heroku_h5sc1hpx?retryWrites=true&w=majority";
+var databaseUri = "mongodb://heroku_h5sc1hpx:jer7765m@cluster-h5sc1hpx.enbxn.mongodb.net/heroku_h5sc1hpx?retryWrites=true&w=majority";
 
 
 if (!databaseUri) {
@@ -25,7 +25,7 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb+srv://heroku_h5sc1hpx:%21jer7765m@cluster-h5sc1hpx.enbxn.mongodb.net/heroku_h5sc1hpx?retryWrites=true&w=majority',
+  databaseURI: databaseUri || 'mongodb://heroku_h5sc1hpx:%21jer7765m@cluster-h5sc1hpx.enbxn.mongodb.net/heroku_h5sc1hpx?retryWrites=true&w=majority',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || '1j5AUs95Nx9y4fbZJEXZNiOVEjcbJjalTFbxMGFQ',
   masterKey: process.env.MASTER_KEY || 'xRbUuRWOv21pvLmBddf9gTcmcnyX1R9AHkjHNRnb', //Add your master key here. Keep it secret!
